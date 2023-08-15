403bd590bd8d2bde694ba5c8ec234fa352568ca9
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-08-15 19:17:59 +0530

    C++ Projects

---
+++ Variables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+//using namespace std;
+int main(){
+    int number = 32, no2 = 23;
+    //int number2 = 23;
+    float pi = 3.3;
+    char r = 'r';
+    std::cout << "this is line of code " <<number<<", and second line of code is "<<no2;
+    std::cout << "this is pi value " << pi;
+    std::cout << "this is a rter " << r;
+
+    return 0;
+} // namespace std;int main()
